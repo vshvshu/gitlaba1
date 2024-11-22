8230c217c110312fb01e7fd2263ea5eba30b1b37
Vasilisa <vasilisa311005@gmail.com>
2024-11-22 16:12:24 +0500

    Начальное состояние

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+fun1()
+{
+
+}
+
+
+void fun2()
+{
+
+}
+
+void fun3()
+{
+
+}
+
+int main()
+{
+    fun1();
+    fun2();
+    fun3();
+}
