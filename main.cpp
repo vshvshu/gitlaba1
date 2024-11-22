286f6589145e5150712ff0ff29e0d4f8cd471e5d
kirusy <ivancerepanov78620@gmail.com>
2024-11-22 16:37:16 +0500

    вызываем функцию 3 от vector3

--- main.cpp
+++ main.cpp
@@ -21,7 +21,13 @@ void f3(vector <string> array)
 ifstream in("labagitt.txt");
 ofstream out;
 out.open("out_git.txt");
-
+if (out.is_open())
+{
+    for (int i = 0; i < array.size(); i++)
+    {
+        out << array[i] << " ";
+    }
+}
 }
 
 int main()
